96df5aa9099a2134f0c0d6cff538ca3e86922175
somdeep <sd2988@columbia.edu>
2016-04-10 18:42:49 -0400

    basic array structure working, checked with insert

--- build.c
+++ build.c
@@ -11,11 +11,12 @@ int main(int argc, char* argv[])
 		exit(0);
 	}
 
-	int k=atoi(argv[1]);
-	int p=atoi(argv[2]);
+	int keys=atoi(argv[1]);
+	int probes=atoi(argv[2]);
 	
 	int levels=argc-3;
 	int i=0;
+	int j=0;
 	int fanout[levels];
 
 	while(i<levels)
@@ -34,21 +35,56 @@ int main(int argc, char* argv[])
 
 	int numkey=levels-1;
 
-	printf("Passed k:%d p:%d levels:%d\n",k,p,levels);
+	printf("Passed k:%d p:%d levels:%d\n",keys,probes,levels);
 
 	int *levelArray[levels];
 	int arraySize[levels];
 	int prev=1;
+	int treesize=0;
 
 	//now building array of arrays
 	for(i=0;i<levels;i++)
 	{
 		arraySize[i]=prev*(fanout[i]-1);
-		
+		treesize+=arraySize[i];
+		//allocate size
+		levelArray[i]=malloc(arraySize[i]*sizeof(int));
 		prev=arraySize[i]+arraySize[i]/(fanout[i]-1);
-		
 		printf("%d\n",arraySize[i]);
 	}
+
+	if(keys>treesize)
+	{	
+		printf("Too many keys\n");
+		exit(1);
+	}
+
+
+	int* value;
+	//check for insert
+	// for (i = 0; i <levels ; i++)
+	// {
+	// 	value=levelArray[i];
+	// 	for (j = 0; j < arraySize[i]; j++)
+	// 	{
+	// 		*value=j;
+	// 		value++;
+	// 	}
+	// }
+	// for (i = 0; i<levels; ++i)
+	// {
+	// 	value=levelArray[i];
+	// 	for (j = 0; j < arraySize[i]; ++j)
+	// 	{
+	// 		printf("%d ", *value);
+	// 		value++;
+	// 	}
+	// 	printf("\n");
+	// }
+
+	
+
+
 
 
 	return(0);
